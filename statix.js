ac3a14f06662c3df30b5293c558c104f976dbebf
Saurabh Sharma <saurabh.19@outlook.com>
2018-12-29 15:23:45 +0530

    added compilation for pug pages

--- statix.js
+++ statix.js
@@ -3,35 +3,32 @@ const frontmatter = require("front-matter");
 const pug = require("pug");
 const marked = require("marked");
 const path = require("path");
-const touch = require("touch");
 
 function main() {
-  const files = fs.readdirSync(`${__dirname}/src/posts`);
+  const post_files = fs.readdirSync(`${__dirname}/src/posts`);
   const template = fs.readFileSync("./src/pages_template.pug", "utf8");
 
   // build MD files
-
-  for (file of files) {
+  // this is where the post are made in html pages
+  for (file of post_files) {
     if (!file.endsWith(".md")) continue;
     let data = fs.readFileSync(`./src/posts/${file}`, "utf8");
     let format = frontmatter(data);
     let obj = { ...format.attributes, body: marked(format.body) };
     let filedata = pug.render(template, { ...obj });
     let filename = file.replace(".md", ".html");
-    // touch.sync(`${__dirname}/build/blog/${filename}`);
     fs.writeFileSync(path.join("build", filename), filedata);
   }
 
   // build HTML files
-
-  for (file of files) {
-    if (!file.endsWith(".md")) continue;
-    let data = fs.readFileSync(`./src/posts/${file}`, "utf8");
-    let format = frontmatter(data);
-    let obj = { ...format.attributes, body: marked(format.body) };
-    let filedata = pug.render(template, { ...obj });
-    let filename = file.replace(".md", ".html");
-    // touch.sync(`${__dirname}/build/blog/${filename}`);
+  // pages are pug files that need to be compiled
+  const page_files = fs.readdirSync(`${__dirname}/src/pages`);
+  console.log(page_files);
+  for (file of page_files) {
+    if (!file.endsWith(".pug")) continue;
+    let data = fs.readFileSync(`./src/pages/${file}`, "utf8");
+    let filedata = pug.render(data);
+    let filename = file.replace(".pug", ".html");
     fs.writeFileSync(path.join("build", filename), filedata);
   }
 }
